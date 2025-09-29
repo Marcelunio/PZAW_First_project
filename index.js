0789ccbd4070843f36da54f9a2d0b25de50e59af
Marcel Zieliński <marcelzielinski.prywatny@gmail.com>
2025-09-29 07:07:10 +0000

    Yes

---
+++ index.js
@@ -0,0 +1,26 @@
+import { createServer } from 'node:http';
+import { URL } from "node:url";
+
+// Create a HTTP server
+const server = createServer((req, res) => {
+  const request_url = new URL(`http://${host}${req.url}`);
+  const path = request_url.pathname;
+  console.log(`Request: ${req.method} ${path}`);
+
+  if (path === "/" && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('hello world!\n');
+  }
+
+  if (!res.writableEnded) {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Site not found!\n');
+  }
+  
+});
+const port = 8000;
+const host = "localhost";
+
+// Start the server
+server.listen(port, host, () => {
+    console.log(`Server listening on http://${host}:${port}`)});
